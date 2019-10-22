d874d8bd12e6c64b761f6885777babf587ec6e2e
babriatis <31776981+babriatis@users.noreply.github.com>
2019-10-22 10:51:23 -0400

    Added code

---
+++ Bop-It/Bop-It.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(24, OUTPUT);     // Speaker Output
+  pinMode(23, INPUT);      // Slide Switch Input
+  pinMode(4, INPUT);       // Button Input
+  pinMode(5, INPUT);       // Toggle Input
+  pinMode(6, OUTPUT);      // Red LED
+  pinMode(11, OUTPUT);     // Green LED
+  pinMode(13, OUTPUT);     // Yellow LED 1
+  pinMode(14, OUTPUT);     // Yellow LED 2
+  pinMode(15, OUTPUT);     // Yellow LED 3
+  pinMode(16, OUTPUT);    // Yellow LED 4
+  pinMode(17, OUTPUT);    // Yellow LED 5
+  pinMode(18, OUTPUT);    // Yellow LED 6
+  pinMode(19, OUTPUT);    // Yellow LED 7
+}
+
+void loop() {
+  
+}
